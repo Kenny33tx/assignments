fe6a2133d4baf6f75bc88483596db84278116997
mckenneth parrish-smth <mckenneth.smith@gmail.com>
2024-07-07 12:11:58 -0600

    Completed react js portion of assignment

---
+++ Level3/Friends with Pets/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Pets from './Pets.jsx'
+import friendsData from './friendsData.js'
+
+const Pet = ({ name, breed }) => (
+  <div className="pet">
+    <p>Pet Name: {name}</p>
+    <p>Breed: {breed}</p>
+    
+  </div>
+);
+
+const Friend = ({ name, age, pets }) => (
+<div className="Friend">
+    <h2>{name}</h2>
+    <p>Age: {age}</p>
+    <h3>Pets:</h3>
+    {pets.map((pet, index) => (
+      <Pet key={index} {...pet} />
+    ))}
+  </div>
+);
+
+
+const FriendList = () => (
+  <div>
+    <h1>Friend List</h1>
+    {friendsData.map((friend, index) => (
+      <Friend key={index} {...friend} />
+    ))}
+  </div>
+);
+
+function App() {
+
+  // const friendElements =friendsData.map((friend,index) =>{
+  //   return <Friends name={friendsData.name}
+  //  age={friendsData.age}/>
+  //  })
+  
+
+
+
+
+return (
+  <div>
+   
+<FriendList/>
+
+
+  </div>
+
+)
+}
+
+export default App
---
+++ Level3/Friends with Pets/src/Friends.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Friends(props){
+ 
+    
+      
+    return(
+        <div>
+           <p>name: {props.name}</p> 
+           <p>age: {props.age}</p>
+         
+        </div>
+    )
+}
